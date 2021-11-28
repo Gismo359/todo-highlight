151aaff9dbf86b30d252f089638e818e405ca18e
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2021-11-28 11:01:34 +0200

    fix: Optimize by recreating regular expressions only when reloading the configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-import path = require('path');
 import * as vscode from 'vscode';
 import {
 	Range,
@@ -6,7 +5,8 @@ import {
 	ExtensionContext,
 	DecorationOptions,
 	TextEditorDecorationType,
-	DecorationRenderOptions
+	DecorationRenderOptions,
+	TextDocument
 } from 'vscode';
 
 interface Dictionary<T> {
@@ -19,10 +19,12 @@ interface AnnotationOptions extends DecorationRenderOptions {
 }
 
 interface Language {
-	languageIds: Array<string>
+	languageIds: Array<string>;
 	lineComments: Array<string>;
 	blockComments: Array<string>;
 	skippedBlocks: Array<string>;
+	combinedCommentRegex?: RegExp;
+	combinedAnnotationRegex?: RegExp;
 }
 
 class DecorationType {
@@ -45,6 +47,28 @@ class GlobalState {
 	private constructor(languages: Array<Language>, decorations: Dictionary<DecorationType>) {
 		this.languages = languages;
 		this.annotations = decorations;
+	}
+
+	private static getAnnotationRegex(info: Language): RegExp {
+		const globalState = GlobalState.getInstance();
+		const annotations = globalState.annotations;
+
+		// TODO@Daniel:
+		//   Add language-specific annotations
+		const patterns = Object.values(annotations).map(t => `(?<${t.name}>${t.pattern})`);
+		const pattern = patterns.join("|");
+		return new RegExp(pattern, "g");
+	}
+	
+	private static getCommentRegex(info: Language): RegExp {
+		const lineComments = info.lineComments.join("|");
+		const blockComments = info.blockComments.join("|");
+		const skippedBlocks = info.skippedBlocks.join("|");
+		const pattern = [
+			`(?<body>${lineComments}|${blockComments})`,
+			`(?:${skippedBlocks})`
+		].join("|");
+		return new RegExp(pattern, "g");
 	}
 
 	public static getInstance(): GlobalState {
@@ -74,97 +98,107 @@ class GlobalState {
 
 		GlobalState.instance = new GlobalState(languages, decorations);
 
-		decorateActiveEditor();
+		for (const language of languages) {
+			language.combinedCommentRegex = GlobalState.getCommentRegex(language);
+			language.combinedAnnotationRegex = GlobalState.getAnnotationRegex(language);
+		}
+
+		decorateVisibleEditors();
 	}
 
 	public languages: Array<Language>;
 	public annotations: Dictionary<DecorationType>;
 }
 
-function getAnnotationRegex(info: Language): RegExp {
-	// TODO@Daniel:
-	//   Add language-specific annotations
-	const patterns = Object.values(GlobalState.getInstance().annotations).map(t => `(?<${t.name}>${t.pattern})`);
-	const pattern = patterns.join("|");
-	return new RegExp(pattern, "g");
-}
-
-function getCommentRegex(info: Language): RegExp {
-	const lineComments = info.lineComments.join("|");
-	const blockComments = info.blockComments.join("|");
-	const skippedBlocks = info.skippedBlocks.join("|");
-	const pattern = [
-		`(?<body>${lineComments}|${blockComments})`,
-		`(?:${skippedBlocks})`
-	].join("|");
-	return new RegExp(pattern, "g");
-}
-
-function decorate(editor: TextEditor) {
-	const document = editor.document;
-
+function findAnnotations(document: TextDocument, info: Language, text: string, offset: number) {
 	const globalState = GlobalState.getInstance();
+	const annotations = globalState.annotations;
 
-	for (const info of globalState.languages) {
-		if (info.languageIds.includes(document.languageId)) {
+	const regex = info.combinedAnnotationRegex;
+	if (regex === undefined) {
+		return;
+	}
+
+	let match;
+	while ((match = regex.exec(text)) !== null) {
+		const groups = match.groups;
+		if (groups === undefined) {
 			continue;
 		}
 
-		const commentRegex = getCommentRegex(info);
-		const annotationRegex = getAnnotationRegex(info);
-
-		const text = document.getText();
-
-		for (const value of Object.values(globalState.annotations)) {
-			value.options = [];
-		}
-
-		let commentMatch;
-		while ((commentMatch = commentRegex.exec(text)) !== null) {
-			const commentBody = commentMatch.groups?.body;
-			if (commentBody === undefined) {
+		for (const [key, value] of Object.entries(groups)) {
+			if (value === undefined) {
 				continue;
 			}
 
 			// NOTE@Daniel:
 			//   Empty matches do not increment lastIndex for some dumb reason
-			if (commentBody === '') {
-				commentRegex.lastIndex++;
+			if (value === '') {
+				regex.lastIndex++;
 			}
 
-			const commentStart = commentMatch.index;
+			const start = offset + match.index;
+			const stop = offset + match.index + match[0].length;
 
-			let annotationMatch;
-			while ((annotationMatch = annotationRegex.exec(commentBody)) !== null) {
-				const annotationGroups = annotationMatch.groups;
-				if (annotationGroups === undefined) {
-					continue;
-				}
+			const decoration = {
+				range: new Range(
+					document.positionAt(start),
+					document.positionAt(stop)
+				)
+			};
 
-				for (const [key, value] of Object.entries(annotationGroups)) {
-					if (value === undefined) {
-						continue;
-					}
-
-					const annotationStart = commentStart + annotationMatch.index;
-					const annotationStop = commentStart + annotationMatch.index + annotationMatch[0].length;
-
-					const startPos = document.positionAt(annotationStart);
-					const stopPos = document.positionAt(annotationStop);
-					const decoration = { range: new Range(startPos, stopPos) };
-
-					globalState.annotations[key].options.push(decoration);
-				}
-			}
-		}
-
-		for (const result of Object.values(globalState.annotations)) {
-			editor.setDecorations(result.type, result.options);
+			annotations[key].options.push(decoration);
 		}
 	}
 }
 
-function decorateActiveEditor() {
+function findComments(document: TextDocument, info: Language) {
+	const text = document.getText();
+
+	const regex = info.combinedCommentRegex;
+	if (regex === undefined) {
+		return;
+	}
+
+	let match;
+	while ((match = regex.exec(text)) !== null) {
+		const body = match.groups?.body;
+		if (body === undefined) {
+			continue;
+		}
+
+		// NOTE@Daniel:
+		//   Empty matches do not increment lastIndex for some dumb reason
+		if (body === '') {
+			regex.lastIndex++;
+		}
+
+		findAnnotations(document, info, body, match.index);
+	}
+}
+
+function decorate(editor: TextEditor) {
+	const document = editor.document;
+	const globalState = GlobalState.getInstance();
+
+	for (const value of Object.values(globalState.annotations)) {
+		value.options = [];
+	}
+
+	for (const info of globalState.languages) {
+		if (!info.languageIds.includes(document.languageId)) {
+			continue;
+		}
+
+		findComments(document, info);
+	}
+
+	for (const result of Object.values(globalState.annotations)) {
+		editor.setDecorations(result.type, result.options);
+	}
+}
+
+function decorateVisibleEditors() {
 	for (const editor of vscode.window.visibleTextEditors) {
 		if (editor !== undefined && editor !== null) {
 			decorate(editor);
@@ -175,8 +209,8 @@ function decorateActiveEditor() {
 export function activate(context: ExtensionContext) {
 	GlobalState.reload();
 
-	const onTextChangedCommand = vscode.workspace.onDidChangeTextDocument(decorateActiveEditor);
-	const onTextEditorChanged = vscode.window.onDidChangeActiveTextEditor(decorateActiveEditor);
+	const onTextChangedCommand = vscode.workspace.onDidChangeTextDocument(decorateVisibleEditors);
+	const onTextEditorChanged = vscode.window.onDidChangeActiveTextEditor(decorateVisibleEditors);
 	const onConfigurationChanged = vscode.workspace.onDidChangeConfiguration(GlobalState.reload);
 
 	context.subscriptions.push(onTextChangedCommand);
